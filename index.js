1389fc1b0f955c2db3171d36dae2e405f230a3f4
surazkarn <surazkarn8848@gmail.com>
2023-06-04 00:42:33 +0530

    socket connection done

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express')
+const http = require('http');
+const socketio = require('socket.io');
+
 const app = express()
+const server = http.createServer(app);
+const io = socketio(server);
 
 app.use("/", express.static(__dirname + '/public'));
 
-app.listen(3006,()=> {
+server.listen(3006,()=> {
     console.log('server started');
 })
